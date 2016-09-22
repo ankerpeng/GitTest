f37a57baffbc785cd1ce7d5d7cdafef7e1bc7a0d
pengxingsong <pengxingsong@able-elec.com>
2016-09-22 16:08:46 +0800

    111

--- 当当网案例/js/header.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-function getE(id){
-	return document.getElementById(id);
-}
-
-//我的当当脚本结束
--- 当当网案例/js/login.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-function tijiao(){
-	var e=document.getElementById("email");
-	var p=document.getElementById("pwd");
-	if(e.value==""|| p.value==""){
-		alert("邮箱和密码不能为空！");
-	}
-}
